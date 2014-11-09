e08842bf15644e05ae11306b883ffb10a7214eff
Ann Catton <anncatton@gmail.com>
2014-11-09 01:10:53 -0500

    created models and views folders, starting to work with sinatra for web version. changed json_test.rb to stations.rband added ARGV function that allows multiple inputs from command line that sends requests to api, instead of local files.

---
+++ stations.rb
@@ -0,0 +1,188 @@
+require "active_support/core_ext/object/to_query.rb"
+require "json"
+require "open-uri"
+require "uri"
+require "byebug"
+
+# so now i need a way to input files not from the command line? or just find a way to input more than one file from the command line
+# the question is, do you want your app to gather a bunch of data from different locations, then compare to the origin station, or take
+# the data from the origin, and search out locations that match the given criteria? i think either way you're going to end up having to
+# access a lot of data
+class Station
+
+	attr_reader :id, :time, :temp, :dewpoint, :humidity, :conditions, :name, :state
+
+	# keep in mind that you'll probably need to change these parameters for a couple reasons - it's simpler to skip dewpoint and humidity
+	# and just use 'feelslike' + weatherShort, and also maybe measurements like dewpoint aren't available at enough locations. you could run some tests
+	# that check to see how many stations come back as not valid (and why) and decide based on that what the best measurement strategy is
+	# you don't want to be rejecting a ton of interesting places just because you're missing a piece of data that could be replaced by 
+	# another one that could serve the same purpose. although i would imagine if things like humidity and dewpoint aren't measured, feelslike temp
+	# is probably also null. * i just found a case where this isn't true - no dewpoint or humidity, but a feelslike measure. what are they using to measure
+	# feelslike temp?
+	# at some point you're going to have to find out what the most accurate system is - the actual separate measurements, or relying on things like
+	# feelslike etc. also thinking that different sites will have different systems for feelslike. that's why i like the separate measurements better.
+	def initialize(id, time, temp, dewpoint, humidity, conditions, name, state)
+		@id = id
+		@time = time
+		@temp = temp
+		@dewpoint = dewpoint
+		@humidity = humidity
+		@conditions = conditions
+		@name = name
+		@state = state
+	end
+
+	def to_s
+		name
+	end
+
+	def print
+		puts "Location: #{@id}"
+		puts "Time observed: #{@time}"
+		puts "Temperature: #{@temp} C"
+		puts "Dewpoint: #{@dewpoint} C"
+		puts "Relative humidity: #{@humidity} %"
+		puts "Current conditions: #{@conditions}"
+		puts
+	end
+
+	def self.from_hash(hash)
+		observations = hash['ob']
+		self.new(
+			hash['id'], 
+			observations['dateTimeISO'], 
+			observations['tempC'], 
+			observations['dewpointC'], 
+			observations['humidity'], 
+			observations['weatherShort'], 
+			hash['place']['name'],
+			hash['place']['state']
+			)
+	end	
+
+# set up a validity check that rejects the bad ones, as opposed to selecting the good ones, so the bad ones are just tossed?
+# have a method that gets all the data, then sorts it. then, start the comparisons. ??
+# andy suggestec just starting out by gathering the data and storing it in files, then assessing it. i wonder if there's a way
+# to just get the measurements i need, not all of them?
+	def not_valid?
+		temp.nil? || dewpoint.nil? || humidity.nil? || conditions.nil?
+	end
+
+	def valid?
+		!temp.nil? && !dewpoint.nil? && !humidity.nil? && !conditions.nil?
+		#!(temp.nil? || dewpoint.nil? || humidity.nil? || conditions.nil?) - this could be a less desirable approach because you have to 
+		# read right to the end of the expression to find out what it's saying, as opposed to the one above which is in discrete sections
+		# and is easier to read. this is important in logical statements cuz they can get confusing pretty fast
+	end
+
+	def matches_temp?(other_station)
+		other_station.temp <= (self.temp + 1) && other_station.temp >= (self.temp - 1)
+	end
+
+	def matches_dewpoint?(other_station)
+		other_station.dewpoint <= (self.dewpoint + 1) && other_station.dewpoint >= (self.dewpoint - 1)
+	end
+
+	def matches?(other_station)
+		other_station.conditions == self.conditions && 
+			matches_temp?(other_station) &&
+			matches_dewpoint?(other_station)
+	end
+
+end
+
+# because you're planning on making this more convoluted, it's probably wise to change this to STDIN.gets instead of ARGV (since
+# you wan to get into query type, query parameters, locations, etc from user).
+# maybe you shouldn't worry about this at all and start making it input a user will put into a website!
+# http://api.aerisapi.com/observations/search?query=country:ca&limit=200&client_id=yotRMCnX8QTlcpwPx71pg&client_secret=H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx
+user_request = ARGV
+query_type = ARGV[0]
+locations = ARGV[1..-1]
+
+request = locations.map do |ea|
+	"/" + query_type + "/" + ea
+end
+
+def make_uri(path)
+
+	my_uri = URI::HTTP.build(
+	{
+		:host => "api.aerisapi.com", 
+		:path => path,
+		:query => {
+			:client_id => "yotRMCnX8QTlcpwPx71pg", 
+			:client_secret => "H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx"
+		}.to_query
+	}
+)
+
+end
+
+requests = request.map do |ea|
+	make_uri(ea)
+end
+
+def make_request(uri)
+
+	open(uri) do |f|
+		json_file = f.read
+		parsed_json = JSON.parse(json_file)
+		puts parsed_json
+	end
+
+end
+
+requests.each do |ea|
+	make_request(ea)
+end
+
+# user input for file names (for now), then write those files to a new file that will be opened by the open(weather_input) method
+# i just realized i was trying to combine too many things - you're not going to be putting file names into your uri.build, cuz that
+# doesn't make any sense! you want to just put in locations from the command line, then create a request to be sent to the api
+
+# uri = URI::HTTP.build(
+# 	{
+# 		:host => "api.aerisapi.com", 
+# 		:path => "/observations/toronto,on,ca", 
+# 		:query => {
+# 			:client_id => "yotRMCnX8QTlcpwPx71pg", 
+# 			:client_secret => "H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx"
+# 		}.to_query
+# 	}
+# )
+
+# open(uri) do |f|
+
+# 	json_file = f.read
+# 	parsed_file = JSON.parse(json_file)
+# 	response = parsed_file['response']
+
+# 	# map returns an array
+# 	# so to access more than one file, do i need to redefine stations
+# 	# ok, you get your data from the api. then, you check each station to see if it's provided valid data. then, the valid data is sent to 
+# 	# the app to use for comparison. i think that's better than checking each station for validity with each comparison request
+# 	stations = response.map { |ea| Station.from_hash(ea) }
+# 	#valid_stations = stations.select { |ea| ea.valid? }
+
+# 	valid_stations = stations.reject { |ea| ea.not_valid? }
+
+# 	a_station = valid_stations.first
+# 	matching = valid_stations[1..-1].find_all do |ea|
+# 		a_station.matches?(ea)
+# 	end
+
+#   place_names = matching.map { |ea| ea.name + " " + ea.state }
+#   str = place_names.join(", ") 
+# 	puts "#{a_station} matches #{str}."
+
+# # what does it say if there are no matches? could give options for 'rather similar' but not the same?
+
+# end
+
+# now want to get data from a wider source. put everything in one file or still separate files in one folder?
+# Issues:
+
+# display of station names - you don't want the station name displayed cuz no one's really going to know what those are. you could use
+# them behind the scenes to point to place names that ARE displayed to the user, database style. or you could write a method that converts
+# the weird-sounding place names (like 'leader arpt (aut') to something more recognizable (like "Leader, SK"). although i have no idea how
+# i would do that
--- where.rb
+++ where.rb
@@ -6,20 +6,7 @@ require "byebug"
 
 # can you combine APIs? just to get a larger amount of data?
 
-	# url = 'http://api.aerisapi.com/observations/seattle,wa,us?client_id=yotRMCnX8QTlcpwPx71pg&client_secret=H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx'
-	# buffer = open(url, "UserAgent" => "Ruby-Wget").read - don't need this, api access works either way. useragent is the browser right?
-	# the "UserAgent" => "Ruby-Wget" - is this the kind of thing i can not worry about completely understanding, because it's just how
-	# their api works and all i need to do is plug it into my code, and not get too caught up in the bits and pieces?
-	# looks like it's calling .read on a kind of File.open - although this is a url. but is that the basic concept?
-	# also, why did they call it buffer? probably cuz its the segue between json and ruby
-
-	#convert JSON data into a hash
-	# response = JSON.parse(buffer)
-	# does the above line make the server response - which i think is in json - readable by Ruby?
-	# yes. parse means to take apart and make sense, in whatever language you're using
-	# so when you call parse on JSON you get a hash?
-
-	# url = 'http://api.aerisapi.com/observations/94107?client_id=yotRMCnX8QTlcpwPx71pg&client_secret=H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx'
+# url = 'http://api.aerisapi.com/observations/94107?client_id=yotRMCnX8QTlcpwPx71pg&client_secret=H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx'
 
 # Performing a query against a numerical property is handled slightly differently than a string property in that it is an >= - <= operation. If only the first value is provided, then all results whose value for that property is greater than or equal to (>=) the specified value is returned. The following request would return all storm cells that have an 80% probability or higher of containing hail:
 # /stormcells/closest?query=ob.hail.prob:80
@@ -37,18 +24,12 @@ my_uri = URI::HTTP.build(
 		}.to_query
 	}
 )
-my_uri = 'http://api.aerisapi.com/observations/search?&query=place.state:wa,ob.tempC:7:9&limit=10&client_id=yotRMCnX8QTlcpwPx71pg&client_secret=H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx'
+# my_uri = 'http://api.aerisapi.com/observations/pe,ca?client_id=yotRMCnX8QTlcpwPx71pg&client_secret=H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx'
 
-http://api.aerisapi.com/places/search?query=place.name:seattle,place.state:wa&client_id=yotRMCnX8QTlcpwPx71pg&client_secret=H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx
-
-http://api.aerisapi.com/observations/search?query=temp:4,dewpt:2,rh:87&client_id=yotRMCnX8QTlcpwPx71pg&client_secret=H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx
-# /observations/search?query=state:mn&sort=temp:-1
 open(my_uri) do |f|
 
 	json_string = f.read
 	parsed_json = JSON.parse(json_string)
-
-	#byebug
 
 	weather = parsed_json['response']['ob']['weatherShort']
 	city_name = parsed_json['response']['place']['name']
@@ -56,8 +37,7 @@ open(my_uri) do |f|
 	humidity = parsed_json['response']['ob']['humidity']
 	dewpointC = parsed_json['response']['ob']['dewpointC']
 	feelslikeC = parsed_json['response']['ob']['feelslikeC']
-	# country = response['response']['place']['name']
-	# print "The weather in " + city + " is " + weather + "."
+
 	puts city_name.capitalize
 	puts weather
 	puts tempC.to_s + " C"
@@ -66,17 +46,10 @@ open(my_uri) do |f|
 
 end
 
-# now, try to access values from just one of the hashes. they're all in json format, so you'll have to parse them into ruby.
-# can you parse a whole json file? or should i create a set of smaller hashes with the relevant data and just parse and access
-# those?
 
 # so now, what do you need to do?
 
 # 	- get data on several cities. when do you need to get the data? how many cities will you look for?
-# 	- set up methods that compare values
-# 	- can you search based on values, not locations? like, your request will ask, 'find me locations with the same temp.'
-# 	- decide on tolerance level for comparison
-# 	- try to do this with just one value first (like temp) to simplify
 
 # first, get conditions for user location (and later on, any location the user specifies)
 # enter those values into a hash
