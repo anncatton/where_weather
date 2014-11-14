6372a389475dc65f16ba7b29a92575bedeabcdef
Ann Catton <anncatton@gmail.com>
2014-11-14 00:48:56 -0500

    removed old comments, moved regional query format to notes file

--- stations.rb
+++ stations.rb
@@ -53,10 +53,6 @@ class Station
 			)
 	end	
 
-# set up a validity check that rejects the bad ones, as opposed to selecting the good ones, so the bad ones are just tossed?
-# have a method that gets all the data, then sorts it. then, start the comparisons. ??
-# andy suggestec just starting out by gathering the data and storing it in files, then assessing it. i wonder if there's a way
-# to just get the measurements i need, not all of them?
 	def not_valid?
 		temp.nil? || dewpoint.nil? || humidity.nil? || conditions.nil?
 	end
@@ -101,31 +97,10 @@ class Station
 
 end
 
-# 	uri = URI::HTTP.build(
-# 	{
-# 		:host => "api.aerisapi.com", 
-# 		:path => "/observations/toronto,on,ca",
-# 		# query is whatever comes after the question mark
-# 		:query => {
-# 			:client_id => "yotRMCnX8QTlcpwPx71pg", 
-# 			:client_secret => "H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx"
-# 		}.to_query
-# 	}
-# )
-# file request for specific region:
-# http://api.aerisapi.com/observations/search?query=state:nu&limit=100&client_id=yotRMCnX8QTlcpwPx71pg&client_secret=H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx
-
 # user enters location to match
 # first return values for that location
 # then return locations that match
 # so you'd have to specify that the app needs to look in every data file. how to do that?
-
-# write a method that reads data from each file, and tells you how many stations there are in each one (:id)
-# push each file to an array, then iterate through it to read all the data?
-
-# do i need to make a relative path for the directory name?
-# puts Dir.foreach("weather_data") { |filename| }
-# uri = ARGV[0]
 
 stations = []
 
