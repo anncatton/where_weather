485d4f6f572e2c8924eb09ad1adeeee3abc2fff9
Ann Catton <anncatton@gmail.com>
2014-11-25 11:24:41 -0500

    corrected empty? to nil? when checking if each station_name has an ICAO code.

--- csv_test.rb
+++ csv_test.rb
@@ -1,11 +1,13 @@
 require "csv"
 
-station_csv = CSV.read('practice.csv', :encoding => 'windows-1251:utf-8', :headers => true)
+station_csv = CSV.read('./stations.csv', :encoding => 'windows-1251:utf-8', :headers => true)
 
 station_names = station_csv.map do |ea|
 	ea.to_hash
 end
 
 station_names.each do |ea|
-	puts "The station code is #{ea['icao']} for #{ea['city']}, #{ea['region']}."
+	unless ea['icao'].nil?
+		puts "The station code is #{ea['icao']} for #{ea['city']}, #{ea['region']}."
+	end
 end
