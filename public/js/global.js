942458a104d33d2309ec36cc640de715d7a02fa1
Ann Catton <anncatton@gmail.com>
2014-12-24 16:24:41 -0800

    organized code a bit better. created function to fetch city and region data from new endpoint, and return as console.log message.

--- public/js/global.js
+++ public/js/global.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
 
-// this is good
-	$("#locations").on("select focus", function(event) {
-		 if (this.value=="Start typing your location") this.value="";
-	});
-
-	// $("#locations").focus(function(event) {
-	// 	var $target = $(event.target);
-	// 	$target.siblings('.cities').slideDown(300);
-	// });
+	function on_enter(target) {
+	  $(target).on("keyup", function(e) {
+	  if(e.which == 13)
+	    $(this).trigger("enter");
+		});
+	}
 
 	function dropMenu(target) {
 		var $target = $(target);
-		$target.siblings('.cities').slideDown(300);
+		$target.siblings(".cities").slideDown(300);
 	}
 
 	function raiseMenu(target) {
@@ -20,28 +17,64 @@ $(document).ready(function() {
 		$target.siblings(".cities").slideUp(300);
 	}
 
-		$("#locations").blur(function(event) {
-	 		var $target = $(event.target);
-	 		$target.siblings('.cities').slideUp(300);
-	 	});
-// maybe: need it to slideUp if $key.length < 3
+	function fetchRegionAndCountry(query) {
+		$.get('/location_search', {query: query}, function(data) {
+			console.log("You entered " + data.city + " " + data.region);
+			// do something with this data;
+		});
+	}
+
+	$("#locations").on("select focus", function(event) {
+		 if (this.value=="Start typing your location") this.value="";
+	});
+
+	$("#locations").blur(function(event) {
+		var $target = $(event.target);
+		$target.siblings(".cities").slideUp(300);
+	});
+
  	$("input").keyup(function(event) {
  		var $target = $(event.target);
- 		var $key = $target.val();
- 		if ($key.length >= 3) 
+ 		var query = $target.val();
+ 		if (query.length >= 3) 
  			dropMenu("input");
  		else
  			raiseMenu("input");
  	});
- 	// 
- 		// so now you need it to look for a match after you've got the 3 characters.
- 		// the event fires after the 4th key is pressed, so i guess the value doesn't change until the next keypress
 
-// when does a variable need to be created as a jquery object? i would assume just when you want to perform jquery actions on it...?
-	$("#current_conditions").mouseenter(function(event) {
-		var $city = $("input").val();
-		console.log("You entered " + $city);
-	});
+ 	$("input").blur(function(event) {
+  	var $target = $(event.target);
+  	var query = $target.val();
+ 		fetchRegionAndCountry(query);
+ 		// send the value of input to the server to return data for that value
+ 	});
+
+// 10.2 (284.55)
+// 15.5 (831.97) 46 .125
+// 13.5 (925.57) 51 .14
+
+
+ // function updateCreditTotalsFromServer(cardType, values) {
+ // 	$.post('/update', {updatedCard: cardType, values: values}, function(data) {
+ // 		var $total = $('#' + cardType + '_total');
+	//  	$total.html(data.total);
+ // 	});
+ // }
+
+//  	$.ajax({
+//   url: '/location_search',
+//   data: ,
+//   success: success,
+//   dataType: dataType
+// });
+
+// .load( url [, data ] [, complete ] )
+// the value of the input will be what's used to search the 'database' for a matching station
+
+	// $("#current_conditions").mouseenter(function(event) {
+	// 	var $city = $("input").val();
+	// 	console.log("You entered " + $city);
+	// });
 
 // $( "#results" ).on( "mouseenter", {
 //     city: "Toronto",
@@ -66,48 +99,11 @@ $(document).ready(function() {
 	// 	}
 	// });
 
-// this does return the alert window twice
-// $("input").blur(function(){
-// 	alert("You entered " + this.value + "!");
-// });
-
-	var city_locations = [
-		{ value: "Toronto", data: "Ontario, Canada" },
-		{ value: "Paris", data: "France" },
-		{ value: "Calgary", data: "Alberta, Canada" },
-		{ value: "Loon Lake", data: "Saskatchewan, Canada" },
-		{ value: "Dallas", data: "Texas, United States" },
-		{ value: "Rome", data: "Italy" },
-		{ value: "Istanbul", data: "Turkey" },
-		{ value: "Copenhagen", data: "Denmark" },
-		{ value: "Punta Catalina", data: "Chile" },
-		{ value: "Mumbai", data: "India" },
-		{ value: "Vancouver", data: "British Columbia, Canada" },
-		{ value: "Kuala Lumpur", data: "Malaysia" },
-		{ value: "Moscow", data: "Russia" },
-		{ value: "Abu Dhabi", data: "United Arab Emirates" },
-		{ value: "Tokyo", data: "Japan" },
-		{ value: "Monrovia", data: "Liberia" },
-		{ value: "Munich", data: "Germany" },
-		{ value: "New York", data: "New York, United States" },
-		{ value: "San Francisco", data: "California, United States" },
-		{ value: "Brisbane", data: "Queensland, Australia" },
-		{ value: "Halifax", data: "Nova Scotia, Canada" },
-		{ value: "McMurdo Station", data: "Antarctica" },
-		{ value: "Tabarka", data: "Tunisia" },
-		{ value: "Brasov", data: "Romania" },
-		{ value: "Edinburgh", data: "Scotland, United Kingdom" },
-		{ value: "London", data: "England, United Kingdom" },
-		{ value: "Shanghai", data: "China" },
-		{ value: "Atalaya", data: "Peru"}
-	];
-
 });
 
 // starting an autocomplete menu from scratch. what do you need?
 // drop down menu
 // regexp? the other guy had a grep command, which is similar, right?
-// minimum characters (3)
 // You can build your own that does not depend on JQuery UI, its a very simple idea of trigger field onchange(), issue an AJAX call to get result that matches what you typed so far, and populate some field with a div or drop down below or near it. And on select of the div or drop down, you populate your trigger field with selected value.
 // create a drop down that is populated from where?
 // if i'm doing an ajax request to find matches for the dropdown, where is that request being sent?
