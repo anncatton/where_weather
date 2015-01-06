d8d2cb87752e7d4addaa88a66c4444b1dfb60aff
Ann Catton <anncatton@gmail.com>
2015-01-06 01:20:26 -0500

    removed obsolete code. restored raiseMenu function on locations input blur.

--- public/js/global.js
+++ public/js/global.js
@@ -17,31 +17,6 @@ $(document).ready(function() {
 		$("#search_results").hide();
 	}
 
-// // right now your where_app select method is returning an array of matches, which you then turn into json, and that becomes
-// // 'data' in these .get functions you're using here
-// 	function populateDropDown(query) {
-// 		// call .html on these 3 parameters to write their values to <li>
-// 		$.post('/location_search', {query: query}, function(data) {
-// 			// var $target = $("li");
-// 			$("ul").empty();
-// 				if (data.length === 0) {
-// 					var element = document.createElement("li");
-// 					var $target = $(element);
-// 					$("ul").prepend($target);
-// 						$target.html("That city is not in the database.");				
-// 				} else {
-// 					data.forEach(function(ea) {
-// 						var element = document.createElement("li");
-// 						var $target = $(element);
-// 						$("ul").prepend($target);
-// 						$target.html(ea.city + ", " + ea.region + ", " + ea.country);
-// 					});
-// 				}
-// 		});
-// 	}		
-
-	// right now your where_app select method is returning an array of matches, which you then turn into json, and that becomes
-// 'data' in these .get functions you're using here
 	function populateDropDown(query) {
 		raiseMenu();
 		$.post('/location_search', {query: query}, function(data) {
@@ -55,25 +30,13 @@ $(document).ready(function() {
 	});
 
 	$("#locations").blur(function(event) {
-		var $target = $(event.target);
-		$target.siblings(".cities").slideUp(300);
+		raiseMenu();
 	});
-
- 	// $("input").keyup(function(event) {
- 	// 	var $target = $(event.target);
- 	// 	var query = $target.val();
- 	// 	if (query.length >= 3) {
- 	// 		dropMenu("input");
- 	// 	}
- 	// 	else {
- 	// 		raiseMenu("input");
- 	// 	}
- 	// });
 
  	$("input").keyup(function(event) {
  		var $target = $(event.target);
  		var query = $target.val();
- 		if (query.length >= 4) {
+ 		if (query.length >= 3) {
  			populateDropDown(query);
  		}
  	});
