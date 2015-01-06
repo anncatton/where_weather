04be18768ad7e66b80d0e7f726ad0d4aa2d2d96e
Ann Catton <anncatton@gmail.com>
2015-01-06 00:56:38 -0500

    removed code that generates html for drop down list, and put in partial templates. drop and raise menu functions now called inside .post function, to better align menu action with data request. drop and raise now use show and hide, not slideUp/slideDown. if/else for location search now in controller file. generally tidied and simplified.

--- public/js/global.js
+++ public/js/global.js
@@ -9,14 +9,12 @@ $(document).ready(function() {
 	// 	});
 	// }
 
-	function dropMenu(target) {
-		var $target = $(target);
-		$target.siblings(".cities").slideDown(300);
+	function dropMenu() {
+		$("#search_results").show();
 	}
 
-	function raiseMenu(target) {
-		var $target = $(target);
-		$target.siblings(".cities").slideUp(300);
+	function raiseMenu() {
+		$("#search_results").hide();
 	}
 
 // // right now your where_app select method is returning an array of matches, which you then turn into json, and that becomes
@@ -45,24 +43,12 @@ $(document).ready(function() {
 	// right now your where_app select method is returning an array of matches, which you then turn into json, and that becomes
 // 'data' in these .get functions you're using here
 	function populateDropDown(query) {
+		raiseMenu();
 		$.post('/location_search', {query: query}, function(data) {
-				if (data.length === 0) {
-					var element = document.createElement("li");
-					var $target = $(element);
-					$("#search_results").html($target);
-						$target.html("That city is not in the database.");				
-				} else {
-					$("#search_results").empty();
-					data.forEach(function(ea) {
-						var element = document.createElement("li");
-						var $target = $(element);
-						$target.html(ea.city + ", " + ea.region + ", " + ea.country);
-						$("#search_results").append($target);					
-					});
-				}
+				$("#search_results").html($(data.html));
+				dropMenu();					
 		});
 	}
-
 
 	$("#locations").on("select focus", function(event) {
 		 if (this.value=="Start typing your location") this.value="";
@@ -73,21 +59,17 @@ $(document).ready(function() {
 		$target.siblings(".cities").slideUp(300);
 	});
 
- 	$("input").keyup(function(event) {
- 		var $target = $(event.target);
- 		var query = $target.val();
- 		if (query.length >= 3) {
- 			dropMenu("input");
- 		}
- 		else {
- 			raiseMenu("input");
- 		}
- 	});
-// wait - you're trying to create <li> elements for each match, AND trying to do a dropMenu, raiseMenu? that doesn't make sense...
+ 	// $("input").keyup(function(event) {
+ 	// 	var $target = $(event.target);
+ 	// 	var query = $target.val();
+ 	// 	if (query.length >= 3) {
+ 	// 		dropMenu("input");
+ 	// 	}
+ 	// 	else {
+ 	// 		raiseMenu("input");
+ 	// 	}
+ 	// });
 
-// so now i'm thinking that what you want to happen now, is instead of writing the data to the console, you would use
-// that to populate your dropdown menu
-// maybe make the function thats doing that into a separate function to call once a set of conditions has been met
  	$("input").keyup(function(event) {
  		var $target = $(event.target);
  		var query = $target.val();
@@ -100,32 +82,3 @@ $(document).ready(function() {
 
 // so now what you want is access to an array of all the locations available to be looked up. one thing you will have to consider is all those
 // towns that don't have an observing station and so will be based on nearby stations (i.e. loon lake). these are places you probably could look up on weather underground (for example) but you'd be seeing results from a nearby station
-
-
-	// $("#current_conditions").mouseenter(function(event) {
-	// 	var $city = $("input").val();
-	// 	console.log("You entered " + $city);
-	// });
-
-// $( "#results" ).on( "mouseenter", {
-//     city: "Toronto",
-//     region: "ON, Canada"
-// }, function( event ) {
-//     console.log( "event data: " + event.data.city + ", " + event.data.region );
-// });
-
-// so in this case the separate events become the arguments for that particular element
-	// $("#results").on({
-	// 	mouseenter: function() {
-	// 		alert("You entered results!");
-	// 	},
-	// event: function() {
-	// 	do something();
-	// },
-	// 	mouseleave: function() {
-	// 		alert("You left results!");
-	// 	},
-	// 	click: function() {
-	// 		alert("You clicked on results!");
-	// 	}
-	// });
