c089fee9faea8d333e6e2bae0ab1f46f5f4de7da
Ann Catton <anncatton@gmail.com>
2015-01-11 18:31:43 -0500

    added function to display first match from dropdown in current location div. add new partial for this as well.

--- public/js/global.js
+++ public/js/global.js
@@ -17,11 +17,18 @@ $(document).ready(function() {
 		$("#search_results").hide();
 	}
 
+// don't like that now the menu flicks on and off when you're typing
 	function populateDropDown(query) {
 		raiseMenu();
 		$.post('/location_search', {query: query}, function(data) {
 				$("#search_results").html($(data.html));
 				dropMenu();					
+		});
+	}
+
+	function printLocation(query) {
+		$.post('/location_search', {query: query}, function(data) {
+			$("#query_location").html(data.first_match);
 		});
 	}
 
@@ -31,6 +38,14 @@ $(document).ready(function() {
 
 	$("#locations").blur(function(event) {
 		raiseMenu();
+	});
+
+// right now this doesn't work because the query data is based on your partials, which returns more data than you want. you'll
+// want it to use just the final selection of the user.
+	$("#locations").blur(function(event) {
+		$target = $(event.target);
+		query = $target.val();
+		printLocation(query);
 	});
 
  	$("input").keyup(function(event) {
@@ -45,3 +60,4 @@ $(document).ready(function() {
 
 // so now what you want is access to an array of all the locations available to be looked up. one thing you will have to consider is all those
 // towns that don't have an observing station and so will be based on nearby stations (i.e. loon lake). these are places you probably could look up on weather underground (for example) but you'd be seeing results from a nearby station
+// you're going to want to use something like the haversine formula to find the closest valid station
--- where_app.rb
+++ where_app.rb
@@ -10,19 +10,11 @@ get '/where_weather' do
 
 end
 
-# create a ruby script that will generate a json file from the csv data. but you want it to come out in the below
-# format so you can use your location_search code. and this is all the data you want for now.
-	
-# now i would like it to return observations from the api
-# so what you'd need to have happen is the user puts in the name of a city. that name is sent to the server, and you'll now want it
-# to match to a station id. then that id should be used to make a request to the api for the conditions for that city.
-# the matching is also going to have to become a regex type search because no one's going to type in an exact match. but first start
-# off once again with the preset location list
-# so it goes: enter city -> city name is sent to server -> that name is used to locate the station id in the hash -> that id is
-# returned as data to the method that creates the request for the api ("data") -> that request is sent to the api for the current
-# conditions -> then you use the <%= %> thing to put that data into the html.
+	# get the page to access state and country data that youve previously stored
+	# to display results for the user query somewhere on the page
 
 # get '/location_search' do
+# '/location_search' is an endpoint, not a url. what's the difference?
 post '/location_search' do
 
   content_type :json
@@ -39,5 +31,24 @@ post '/location_search' do
   	erb :_data_field, :layout => false, :locals => { :matches => matches }
 	end
 
-  { :html => content }.to_json # this is what is returned as 'data' in the jquery code. using .select, this will be an array
+# if field is empty this still returns the first city in LOCATIONS, which is Adelaide. using matches.empty? doesn't help
+	first_city = erb :_display_span, :layout => false, :locals => {:first_match => matches.first }
+
+  { :html => content, :first_match => first_city }.to_json # this is what is returned as 'data' in the jquery code. using .select, this will be an array
+
 end
+
+# now need to get your site to access stored data to find observations for the user query. you can use old data for now.
+# the data acquisition will happen independently of the web page
+#	http://api.aerisapi.com/observations/halifax,ns,ca?client_id=yotRMCnX8QTlcpwPx71pg&client_secret=H2Nx8mcIPgZtCBLCV2KRPnh4T6n8LiIXejDMGgQx
+# now use the user input to look up a station id in the api
+# create a ruby script that will generate a json file from the csv data. but you want it to come out in the below
+# format so you can use your location_search code. and this is all the data you want for now.
+# now i would like it to return observations from the api
+# so what you'd need to have happen is the user puts in the name of a city. that name is sent to the server, and you'll now want it
+# to match to a station id. then that id should be used to make a request to the api for the conditions for that city.
+# the matching is also going to have to become a regex type search because no one's going to type in an exact match. but first start
+# off once again with the preset location list
+# so it goes: enter city -> city name is sent to server -> that name is used to locate the station id in the hash -> that id is
+# returned as data to the method that creates the request for the api ("data") -> that request is sent to the api for the current
+# conditions -> then you use the <%= %> thing to put that data into the html.
