58aaead42e952e678503fc2d43cbdb4998ccea61
Ann Catton <anncatton@gmail.com>
2015-02-08 11:22:43 -0500

    removed console.log statements. changed keypress back to keyup on line 34, forgot to do that on last commit.

--- public/js/global.js
+++ public/js/global.js
@@ -22,42 +22,27 @@ $(document).ready(function() {
 	};
 
 // this for the drop down menu
-// don't like that now the menu flicks on and off when you're typing - i think this is just because it's running slow, for every
-// keyup there's a get request
-// would you be able to do version that just did one request, got an array of possible matches, then shifted off the ones that
-// no longer match after each subsequent keyup?
 	function populateDropDown(query) {
 		raiseMenu();
-		console.log('location_search request');
 		$.get('/location_search', {query: query}, function(data) {
-				console.log('location_search has responded');
 				$("#search_results").html($(data.html));
 				dropMenu();	
 		});
 	};
 
 	var timeout;
-	console.log("setting up keypress handler");
- 	$("#location_input").keypress(function(event) { // this might be slowing webpage down cuz it sends a search request for every keyup
+ 	$("#location_input").keyup(function(event) {
 
  		var handleKeyup = function() {
- 			console.log("handling keyup");
     	var $target = $(event.target);
  			var query = $target.val();
  			if (query.length >= 3) {
  				populateDropDown(query);
  			}
  		};
- 		console.log("clear timeout");
  		clearTimeout(timeout);
- 		console.log("setting timeout");
  		timeout = setTimeout(handleKeyup, 1000);
     
  	});
 
 });
-
-// do you use the same approach in js as you would with css - that you try to stick to class and id references, not element types (like <h3>
-// or <span>)?
-// i guess also you have to look at the particular conditions in which you're applying styles - does it make more sense to apply a style to a
-// certain class or id, or would it work better if applied all of a certain element?
