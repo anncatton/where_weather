ba5776b1d59f49e901bed4840551787817accb06
Ann Catton <anncatton@gmail.com>
2015-03-06 12:24:49 -0500

    added selected class. changed all id's to classesfor better extendability. js now adds selected class with down arrow.

--- public/js/global.js
+++ public/js/global.js
@@ -1,48 +1,69 @@
 $(document).ready(function() {
 
-	// var EnterKey = 13;
-
-	// function on_enter() {
-	//   $(target).on("keyup", function(e) {
-	//   if(e.which == EnterKey)
-	//     $(this).trigger("enter");
-	// 	});
-	// }
-
-	$("#location_input").on("select focus", function(event) {
+	$(".location_input").on("select focus", function(event) {
 		 if (this.value=="Start typing your location") this.value="";
 	});
 
 	function dropMenu() {
-		$("#search_results").show();
+		$(".search_results").show();
 	};
 
 	function raiseMenu() {
-		$("#search_results").hide();
+		$(".search_results").hide();
 	};
 
 // this for the drop down menu
 	function populateDropDown(query) {
 		raiseMenu();
 		$.get('/location_search', {query: query}, function(data) {
-				$("#search_results").html($(data.html));
+				$(".search_results").html($(data.html));
 				dropMenu();	
 		});
 	};
 
-	var timeout;
- 	$("#location_input").keyup(function(event) {
+	// function checkKey(event) {
 
- 		var handleKeyup = function() {
-    	var $target = $(event.target);
- 			var query = $target.val();
- 			if (query.length >= 3) {
- 				populateDropDown(query);
- 			}
- 		};
- 		clearTimeout(timeout);
- 		timeout = setTimeout(handleKeyup, 200);
+ //    // if (event.keyCode == '38') {
+ //    //     // up arrow
+ //    // }
+ //    if (event.keyCode == '40') {
+ 						// down arrow
+ //    }
+	// }
+
+
+	var timeout;
+ 	$(".location_input").keyup(function(event) {
+
+ 		var $target = $(event.target);
+
+ 		if (event.keyCode == '40') {
+
+ 			var $targetParent = $target.closest(".location_menu");
+ 			var currentlySelectedListItem = $targetParent.find(".result.selected");
+ 			var listItemToSelect = currentlySelectedListItem.next('.result');
+
+ 			$(currentlySelectedListItem).removeClass("selected");
+ 			$(listItemToSelect).addClass("selected");
+ 			
+ 		} else {
+
+	 		var handleKeyup = function() {
+	 			var query = $target.val();
+	 			if (query.length >= 3) {
+	 				populateDropDown(query);
+	 			}
+	 		};
+	 		clearTimeout(timeout);
+	 		timeout = setTimeout(handleKeyup, 200);
+	 	}
     
  	});
+
+// need to put in a function for using arrow and enter keys to select from dropdown list
+// check to see if down arrow key has been pressed - which number is down arrow?
+// if no list item is selected, select the first one
+// if the first one is selected, select the next one
+// when an item is selected, you add a "selected" class to that list item
 
 });
