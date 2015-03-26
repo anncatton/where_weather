a31853b96f6856e549bd7583b9f0abb47db58118
Ann Catton <anncatton@gmail.com>
2015-03-26 12:53:51 -0400

    changed lat/long from separate integer values to POINT.

--- model.sql
+++ model.sql
@@ -1,3 +1,7 @@
+-- ALTER TABLE stations ADD COLUMN location POINT;
+
+-- UPDATE stations SET location = SELECT CAST((longitude, latitude) AS POINT);
+
 DROP TABLE IF EXISTS weather_data;
 DROP TABLE IF EXISTS stations;
 
@@ -5,13 +9,12 @@ CREATE TABLE stations (
 	name text NOT NULL, 
 	region text, 
 	country text NOT NULL, 
-	latitude integer NOT NULL, 
- 	longitude integer NOT NULL, 
+	location point NOT NULL,
 	id text PRIMARY KEY
 	);
 
 CREATE TABLE weather_data (
-	id bigserial PRIMARY KEY,
+	id bigserial PRIMARY KEY, -- so that you can add each new set of observations to the growing database
 	station_id text NOT NULL REFERENCES stations (id),
  	time text NOT NULL, 
  	temp integer, 
@@ -24,3 +27,11 @@ CREATE TABLE weather_data (
  	wind_kph integer, 
  	wind_direction text
  	);
+
+-- the station_id in weather_data must be in stations in order to remain valid, but a station id (id) in stations does
+-- not need to be in weather_data in order to be valid. so, weather_data is dependent on stations
+-- this is why you have weather_data(station_id) REFERENCES stations(id)
+-- more constraints: station ids must be 4 characters long
+-- Many developers consider explicitly listing the columns better style than relying on the order implicitly. also, i'm
+-- assuming you'll be creating methods to populate your tables, not typing each one in!
+
