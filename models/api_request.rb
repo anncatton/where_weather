abed9e37713d008712d66127b0ea5e1630e297e2
Ann Catton <anncatton@gmail.com>
2015-04-13 19:08:33 -0400

    removed old comments and bad version of matches query

--- models/api_request.rb
+++ models/api_request.rb
@@ -249,40 +249,20 @@ observations = DB[:weather_data]
 # 	end
 # end
 
-	def find_station(id, table)
-		observation_hash = table.all
-
-		match = observation_hash.each do |ea|
-			if ea[:station_id] == id
-				Observation.from_table(ea)
-			end
-			# ea[:station_id] == id
-			# or ea[id] ??
-		end
-
-		match
-	end
-
 def find_station(id)
 	observations = DB[:weather_data]
-	match = observations.where(:station_id=>id.upcase).first # if you call .first you don't get the problem of having just a Dataset object
+	match = observations.where(:station_id=>id.upcase).first
 	match
 end
 
 station_to_match = find_station(my_id)
 station_to_match_data = Observation.from_table(station_to_match)
 
-
-# stations_to_compare = DB[:weather_data].map do |ea|
-# 	Observation.from_table(ea)
-# end
-
 # valid_stations = stations_to_compare.reject do |ea|
 # 	ea.not_valid?
 # end
 
 def matches?(query_station, observations)
-# posts.where('(stamp < ?) AND (author != ?)'
 	observations.where(:temp => (query_station.temp - 1)..(query_station.temp + 1)).where(
 		:dewpoint => (query_station.dewpoint - 1)..(query_station.dewpoint + 1)).where(
 		:humidity => (query_station.humidity - 5)..(query_station.humidity + 5)).where(
@@ -296,14 +276,3 @@ puts all_matches
 
 # items.where(:price => 100..200).sql # calling .sql just gives you the raw SQL which is great!
 #=> "SELECT * FROM items WHERE (price >= 100 AND price <= 200)"
-
-# puts find_station(my_id, observations)
-# my_station = DB[:weather_data].filter(:station_id=>my_id) # this returns a Dataset object, so the search, not the data itself
-# but calling .all on my_station here makes it an array of hashes
-# my_station = observations.where(:station_id=>my_id) # this returns a Dataset object, so the search, not the data itself
-# user_station = observations[:station_id=>my_id] # this returns an array of hashes, tho in this case it's just one hash
-# new_record = Observation.from_table(user_station)
-# puts new_record.temp
-# puts new_record.dewpoint
-# do queries that find the relevant stations first - i.e. the station the user searches for, and the ones that match - and then turn
-# them into instances of Station/Observation so you can use them in your Ruby code. you don't want to do that work for all stations first!
