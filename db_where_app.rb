ab8faa7083ee0f62101f95761d1c5a31ec808963
Ann Catton <anncatton@gmail.com>
2015-04-17 02:48:39 -0400

    logger method working properly. deleted inefficient find_station method. weather value comparisons now happen during database queries, to limit number of records retrieved from db. commented out validity check, don’t think it’s needed anymore because the db queries should ignore any records missing necessary values. renamed some variables to improve readability. removed partial locals that weren’t being used. all data for comparisons and identification now coming from db.

--- db_where_app.rb
+++ db_where_app.rb
@@ -7,12 +7,9 @@ require "pg"
 require "sequel"
 require 'logger'
 
-# DB = Sequel.sqlite '', :loggers => [Logger.new($stdout)]
-# or
-DB.loggers << Logger.new($stdout)
-
 DB = Sequel.connect('postgres://anncatton:@localhost:5432/mydb')
 # DB.sql_log_level = :debug
+DB.loggers << Logger.new($stdout)
 
 get '/' do
 	redirect to('/where_weather')
@@ -20,64 +17,49 @@ end
 
 get '/where_weather' do
 
-	stations = DB[:stations]
-	observations = DB[:weather_data]
-
-	# def find_station(id)
-	# 	observation_hash = observations.all
-	# 	observation_hash.find do |ea|
-	# 		ea[:station_id] == id
-	# 	end
-	# end
+	stations_table = DB[:stations]
+	observations_table = DB[:weather_data]
 	
-	# is this better than the one above? if so, why? it is, because the one above queries for ALL the records in the weather_data table,
-	# whereas this one below makes a more specific query - it looks for records with that station_id, limit 1 result
-	def find_station(id)
-		observations = DB[:weather_data]
-		match = observations.where(:station_id=>id.upcase).first
+	def find_station(id, table)
+		match = table.where(:station_id=>id.upcase).first
 		match
 	end
 	
-	# should i change this to be a search for records that meet the match criteria?
-	# for example WHERE temp =/- 1 AND WHERE humidity = +/- 5 AND etc... YES
-	# sooo... you want to find the records that match the criteria BEFORE you turn them into Observation instances?
-	# so don't do this just below! then you're creating hundreds of instances you don't even need
-	stations_to_compare = DB[:weather_data].map do |ea|
-		Observation.from_table(ea)
-	end
+# not sure you need a valid check cuz the db search should ignore any records that are missing necessary values
+# at some point you'll need to specify that temp, dewpoint and humidity are to be checked first, and then values like
+# windspeed etc are more optional and can be checked on a second run
+	# valid_stations = stations_to_compare.reject do |ea|
+	# 	ea.not_valid?
+	# end
 
-	valid_stations = stations_to_compare.reject do |ea|
-		ea.not_valid?
-	end
-# i've checked up to here
 	if params.empty? # this doesn't currently help when you load page without a query attached in the address bar. guess you'll have to
 		# load it with an autoip query maybe? also _results_view has an if/else to handle matching_station being nil
 		erb :index, :layout => :layout, :locals => { :matching_station => nil,
 																								:locations_match => nil }
 	else
 		station_id = params[:id]
-		station_to_match = find_station(id)
-		locations_match = stations.where(:id=>station_id.upcase).first
+		station_to_match = find_station(station_id, observations_table) # station_to_match_data in api_request
+		match_in_stations_table = stations_table.where(:id=>station_id.upcase).first
+		station_to_match_data = Observation.from_table(station_to_match)
 
-		station = Station.from_json(matching_station)
-		matches = valid_stations.find_all do |ea|
-			ea != station && station.matches?(ea)
+		station = Station.from_stations_table(match_in_stations_table)
+
+		all_matches = matches?(station_to_match_data, observations_table) # an array of hashes from observations table. not yet Observation instances
+		all_matches_in_stations_table = all_matches.map do |ea| # all_matches_in_stations_table is data from stations
+			stations_table.where(:id=>ea[:station_id].upcase).first
 		end
 
-		def find_pretty_match_station(station_to_match)
-			stations = DB[:stations]
-			match = stations.where(:id=>station_to_match.id.upcase).first
-			# match = station_list.find do |ea|
-			# 	match = ea[:id] == station_to_match.id
-			# 	match
-			# end
-
-			match
+		match_stations_data = all_matches_in_stations_table.map do |ea|
+			Station.from_stations_table(ea)
 		end
 
-		erb :index, :layout => :layout, :locals => { :matching_station => matching_station,
-																								:locations_match => locations_match,
-																								:matches => matches }
+		matches = all_matches.map do |ea|
+			Observation.from_table(ea)
+		end
+
+		erb :index, :layout => :layout, :locals => {:station_to_match_data => station_to_match_data,
+																								:station => station,
+																								:match_stations_data => match_stations_data }
 	end
 
 
