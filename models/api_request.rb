c5bb2c5a5ea1d01993725baee8906a4670cfcef7
Ann Catton <anncatton@gmail.com>
2015-04-18 12:51:31 -0400

    removed unnecessary requires. moved everything to do with Observation class including methods to separate file. this file is now just for the api request and writing to json file.

--- models/api_request.rb
+++ models/api_request.rb
@@ -1,65 +1,9 @@
-require "byebug"
 require "json"
 require "open-uri"
 require "uri"
 require "fileutils"
 require "active_support/core_ext/object/to_query.rb"
-require "haversine"
-require "pg"
-require "sequel"
-# require "sinatra"
-# require "./stations.rb"
-
-class Observation
-
-	attr_reader :id, :time, :temp, :dewpoint, :humidity, :conditions, :weather_primary_coded, :clouds_coded, :is_day, :wind_kph, :wind_direction
-
-	def initialize(id, time, temp, dewpoint, humidity, conditions, weather_primary_coded, clouds_coded, is_day, wind_kph, wind_direction)
-		@id = id
-		@time = time
-		@temp = temp
-		@dewpoint = dewpoint
-		@humidity = humidity
-		@conditions = conditions
-		@weather_primary_coded = weather_primary_coded
-		@clouds_coded = clouds_coded
-		@is_day = is_day
-		@wind_kph = wind_kph
-		@wind_direction = wind_direction
-	end
-
-	def self.from_table(hash)
-		self.new(
-			hash[:station_id], # need this for identification, and to join with stations table
-			hash[:time],
-			hash[:temp],
-			hash[:dewpoint],
-			hash[:humidity],
-			hash[:conditions],
-			hash[:weather_primary_coded],
-			hash[:clouds_coded],
-			hash[:is_day],
-			hash[:wind_kph],
-			hash[:wind_direction]
-			)
-	end
-
-# for comparing conditions with coded weather, so that you get matches from day or night
-	# def not_valid?
-	# 	temp.nil? || dewpoint.nil? || humidity.nil? || weather_primary_coded.nil? || conditions.nil?
-	# end
-
-# this is not currently being used. time from api is local, with difference from GMT shown as +/-
-	def matches_time?(other_station)
-		other_station.time <= (self.time + 1) && other_station.time >= (self.time - 1)
-	end
-
-	# def too_close?(station)
-	# 	distance = Haversine.distance(self.latitude, self.longitude, station.latitude, station.longitude)
-	# 	distance.to_km < 2000
-	# end
-
-end
+require "./observation.rb"
 
 # this section is for building requests to the api
 def build_query(query)
@@ -185,84 +129,6 @@ countries = ["ae", "af", "ag", "al", "am", "ao", "aq", "ar", "at", "au", "aw", "
 
 us_and_canada = ["ab", "al", "ak", "az", "ar", "bc", "ca", "co", "ct", "de", "dc", "fl", "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la", "mb", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "nb", "ne", "nv", "nh", "nj", "nl", "nm", "ns", "nt", "nu", "ny", "nc", "nd", "oh", "ok", "on", "or", "pa", "pe", "qc", "ri", "sc", "sd", "sk", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy", "yt"]
 
-# get_all_data(countries, us_and_canada)
+get_all_data(countries, us_and_canada)
 
 # table.insert(:station_id=>station[:station_id], :time=>station[:time], :temp=>station[:temp], :dewpoint=>station[:dewpoint], :humidity=>station[:humidity], :conditions=>station[:conditions], :weather_coded=>station[:weather_coded], :clouds_coded=>station[:clouds_coded], :is_day=>station[:is_day], :wind_kph=>station[:wind_kph], :wind_direction=>station[:wind_direction])
-
-# this section to make an Observation instance from the observations table in mydb
-
-# DB = Sequel.connect('postgres://anncatton:@localhost:5432/mydb')
-
-# # my_city = ARGV.first
-# my_id = ARGV.first
-# observations = DB[:weather_data]
-# stations = DB[:stations]
-
-# stations_data_for_user_id = stations.where(:id => my_id).first
-
-# def get_station_data(id,table)
-# 	table.where(:id => id).first
-# end
-
-# city_data = get_station_data(my_id, stations)
-
-# def find_city_id(city, table)
-# 	id = table.select(:id).where(:name=>city).first
-# end
-
-# puts find_city_id(my_city, stations)
-
-# to find the weather data associated with a station id
-# def find_station(id)
-# 	observations = DB[:weather_data]
-# 	match = observations.where(:station_id=>id.upcase).first
-# 	match
-# end
-
-# station_to_match = find_station(my_id)
-# station_to_match_data = Observation.from_table(station_to_match)
-
-def matches?(query_station, observations)
-	observations.where(:temp => (query_station.temp - 1)..(query_station.temp + 1)).where(
-		:dewpoint => (query_station.dewpoint - 1)..(query_station.dewpoint + 1)).where(
-		:humidity => (query_station.humidity - 5)..(query_station.humidity + 5)).where(
-		:weather_primary_coded => query_station.weather_primary_coded).where(
-		:wind_kph => (query_station.wind_kph - 5)..(query_station.wind_kph + 5)).exclude(
-		:station_id => query_station.id).all
-end
-
-# id_to_match = get_station_data(my_id, stations)
-# # so with too_close, you get all the stations that match, then, using their ids, check stations to see if they're too close
-# all_matches = matches?(station_to_match_data, observations)
-# match_data_array = all_matches.map do |ea|
-# 	Observation.from_table(ea)
-# end
-
-# match_data_ids = all_matches.map do |ea|
-# 	ea[:station_id]
-# end
-
-# # gives an array of station hashes from stations table
-# results = match_data_ids.map do |ea|
-# 	result = stations.where(:id=>ea).first
-# end
-
-# def too_close?(station_to_compare, user_station)
-# 	distance = Haversine.distance(user_station[:latitude], user_station[:longitude], station_to_compare[:latitude], station_to_compare[:longitude])
-# 	distance.to_km < 1000
-# end
-
-# matches = results.each do |ea|
-# 	unless too_close?(ea, stations_data_for_user_id)
-# 		# puts "#{stations_data_for_user_id[:name]}, #{stations_data_for_user_id[:country]} matches #{ea[:name]}, #{ea[:country]}"
-# 		puts ea
-# 	end
-# end
-
-# items.where(:price => 100..200).sql # calling .sql just gives you the raw SQL which is great!
-#=> "SELECT * FROM items WHERE (price >= 100 AND price <= 200)"
-
-# would like get_all_data to write directly to the database, not ->to file and then ->to database
-# search with city name, find corresponding station id in stations, then use that id to search in weather_data to match other cities
-# scoring scheme for matches!
-# get the too_close? method working
