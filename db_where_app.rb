77b1478e9d2414f138c0f8a3568e62545cfc0a74
Ann Catton <anncatton@gmail.com>
2015-05-10 14:18:57 -0400

    add if clauses to check for cases: no recent enough observation for query location (index will then display that there is no current data), and for observations missing temp, dewpoint and/or weather_primary_coded (index will display that there is not enough data).

--- db_where_app.rb
+++ db_where_app.rb
@@ -27,20 +27,13 @@ get '/where_weather' do
 		current_time - 3600
 	end
 	
-	# def find_station(id, table)
-	# 	time_to_compare = time_threshold
-	# 	match = table.where{time >= (time_to_compare - 3600)}.where(:station_id=>id.upcase).first 
-	# 	match
-	# end
 	def find_station_observation(station_id)
 		observations = DB[:weather_data]
 		time_to_compare = time_threshold
-		# looks like that buron obsv was ignored because its AFTER the time_to_compare
 		matching_observation = observations.where{time >= (time_to_compare - 3600)}.where(:station_id => station_id.upcase).first
-		# matching_observation = observations.where(:time => (time_to_compare - 3600)..(time_to_compare + 3600)).where(:station_id => station_id.upcase).first
 		matching_observation
 	end
-#	where(:wind_kph => (wind_kph - 5)..(wind_kph + 5))
+
 # not sure you need a valid check cuz the db search should ignore any records that are missing necessary values
 # at some point you'll need to specify that temp, dewpoint and humidity are to be checked first, and then values like
 # windspeed etc are more optional and can be checked on a second run
@@ -48,51 +41,69 @@ get '/where_weather' do
 	# 	ea.not_valid?
 	# end
 
-	if params.empty? # this doesn't currently help when you load page without a query attached in the address bar. guess you'll have to
-		# load it with an autoip query maybe? also _results_view has an if/else to handle matching_station being nil
+	if params.empty?
 		erb :index, :layout => :layout, :locals => { :matching_station => nil,
 																								:locations_match => nil,
 																								:station => nil }
 	else
 		station_id = params[:id]
-		# station_to_match = find_station(station_id, observations_table) # station_to_match_data in api_request
 		station_to_match = find_station_observation(station_id)
-# byebug
 		match_in_stations_table = stations_table.where(:id=>station_id.upcase).first
-		station_to_match_data = Observation.from_table(station_to_match)
 		station = Station.from_table(match_in_stations_table)
 
-		all_matches = station_to_match_data.find_matches # an array of hashes from observations table. not yet Observation instances
+		# for error when there's no matching observation within the current timeframe (not recent enough)
+		# this uses a db query to filter out observations that aren't recent enough
+		if station_to_match.nil?
+			erb :index, :layout => :layout, :locals => {:station => station,
+																									:station_to_match => station_to_match,
+																									:station_to_match_data => nil,
+																									:matches_to_display => []}
+		else
 
-		matches_within_window = all_matches.reject do |ea|
-			time_to_compare = time_threshold
-			Time.parse(ea[:time]) <= time_to_compare
+			station_to_match_data = Observation.from_table(station_to_match)
+# this is making sure temp, dewpoint and weather conditions coded are present in the query station. can you find this out while querying the db?
+			if station_to_match_data.temp.nil? || station_to_match_data.dewpoint.nil? || station_to_match_data.weather_primary_coded.nil?
+				erb :index, :layout => :layout, :locals => {:station => station,
+																										:station_to_match => station_to_match,
+																										:station_to_match_data => nil,
+																										:matches_to_display => []}
+			else
+				all_matches = station_to_match_data.find_matches # an array of hashes from observations table. not yet Observation instances
+
+				matches_within_window = all_matches.reject do |ea|
+					time_to_compare = time_threshold
+					Time.parse(ea[:time]) <= time_to_compare
+				end
+
+				all_matches_in_stations_table = matches_within_window.map do |ea| # all_matches_in_stations_table is data from stations
+					stations_table.where(:id=>ea[:station_id].upcase).first
+				end
+
+		# this shouldn't be necessary if you add the foreign key reference back into weather_data
+				valid_matches = all_matches_in_stations_table.reject do |ea|
+					ea.nil?
+				end
+
+				match_stations_data = valid_matches.map do |ea|
+					Station.from_table(ea)
+				end
+				
+				matches_not_too_close = match_stations_data.reject do |ea|
+					too_close?(ea, station)
+				end
+
+				# matches = all_matches.map do |ea| # only if you need to display the weather conditions of the matches
+				# 	Observation.from_table(ea)
+				# end
+
+				erb :index, :layout => :layout, :locals => {:station_to_match_data => station_to_match_data,
+																										:station => station,
+																										:station_to_match => station_to_match,
+																										:matches_to_display => matches_not_too_close }
+			end
+
 		end
 
-		all_matches_in_stations_table = matches_within_window.map do |ea| # all_matches_in_stations_table is data from stations
-			stations_table.where(:id=>ea[:station_id].upcase).first
-		end
-
-# this shouldn't be necessary if you add the foreign key reference back into weather_data
-		valid_matches = all_matches_in_stations_table.reject do |ea|
-			ea.nil?
-		end
-
-		match_stations_data = valid_matches.map do |ea|
-			Station.from_table(ea)
-		end
-		
-		matches_not_too_close = match_stations_data.reject do |ea|
-			too_close?(ea, station)
-		end
-
-		# matches = all_matches.map do |ea| # only if you need to display the weather conditions of the matches
-		# 	Observation.from_table(ea)
-		# end
-
-		erb :index, :layout => :layout, :locals => {:station_to_match_data => station_to_match_data,
-																								:station => station,
-																								:matches_to_display => matches_not_too_close }
 	end
 
 
