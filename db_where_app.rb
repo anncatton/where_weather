cdb27212f9bc8370b3960ac348769668ecf93d81
Ann Catton <anncatton@gmail.com>
2015-07-02 14:25:38 -0400

    now calls .uniq on matches to prevent duplicate entries from being displayed.

--- db_where_app.rb
+++ db_where_app.rb
@@ -17,119 +17,98 @@ end
 
 get '/where_weather' do
 
-	# do i want to access DB through these variables or just directly?
-	stations_table = DB[:stations]
-	observations_table = DB[:weather_data]
-	stations_and_observations_join = DB[:stations].join(DB[:weather_data], :station_id => :id)
-
-	def time_threshold
-		current_time = Time.parse("2015-04-20T10:19:00-04:00")
-		current_time - 3600
-	end
+	# do i want to access DB through these variables or just directly? the format only matters if you'll be referencing a db query a lot, then 
+	# you'd want to store it in a variable. if you only need it a few times, it's not worth it
+	# stations_and_observations_join = DB[:stations].join(DB[:weather_data], :station_id => :id)
 	
-	# def find_station_observation(station_id) # this finds the record in observations that is within the time window (the last 60min)
-	# 	observations = DB[:weather_data]
-	# 	time_to_compare = time_threshold
-	# 	# do i need the upcase call on station_id here?
-	# 	matching_observation = observations.where{time >= (time_to_compare - 3600)}.where(:station_id => station_id.upcase).first
-	# 	matching_observation
+	# def time_threshold
+	# 	time_to_compare = Time.parse('2015-06-29 22:00:00-04')
+	# 	time_to_compare - 3600
 	# end
 
-	def find_station_and_observation(station_id)
-		# stations_table = DB[:stations]
-		# observations_table = DB[:weather_data]
-		stations_and_observations_join = DB[:stations].join(DB[:weather_data], :station_id => :id)
-		time_to_compare = time_threshold
-		stations_and_observations_join.where{time >= (time_to_compare - 3600)}.where(:station_id => station_id.upcase).first
-	end
+# this finds the record in observations that is within the time window (the last 60min)
+	# def find_station_and_observation(station_id)
+	# 	stations_and_observations_join = DB[:stations].join(DB[:weather_data], :station_id => :id)
+	# 	time_to_compare = time_threshold
+	# 	stations_and_observations_join.where{time >= (time_to_compare - 3600)}.where(:station_id => station_id.upcase).first
+	# end
 
 	station_id = params[:id]
 
 	if station_id.nil?
-		erb :index, :layout => :layout, :locals => {:station => nil,
-																								:station_to_match => nil,
-																								:station_to_match_data => nil,
-																								:matched_stations_to_display => nil }
-	else
-		station_to_match = find_station_and_observation(station_id)
 
-		if station_to_match.nil? # this saves error if someone types in a random set of letters, numbers, or nothing after id= in the address bar. does not save if someone types where_weather?id without the =
+		erb :index, :layout => :layout, :locals => {:matching_observation => nil}
+
+	else
+
+		matching_observation = Observation.match_in_timeframe(station_id, '2015-07-02 00:00:00', '2015-07-02 02:00:00')
+
+		if matching_observation.nil? # this saves error if someone types in a random set of letters, numbers, or nothing after id= in the address bar. does not save if someone types where_weather?id without the =
 		# also for error when there's no matching observation within the current timeframe (not recent enough), or if there's no observation at all for the queried station (like you found with CYXD). however, this page view doesn't tell the user anything
 		# station_to_match being nil means the station id exists, but there are no observations within the right time frame
 		# station_to_match_data being nil means there is some data missing, but there is an observation record within the right time frame
 		# reading those last 2 lines, your variable names really don't make any sense!
-			station_record = stations_table.where(:id=>station_id.upcase).first
+			station_record = DB[:stations].where(:id=>station_id.upcase).first
 
-			# this one is if the station is not in the stations table
+			# this one is if the station is not in the stations table. you shouldn't need this if weather_data depends on stations
 			if station_record.nil?
 				erb :index, :layout => :layout, :locals => {:station => nil,
-																									:station_to_match => nil,
-																									:station_to_match_data => nil,
-																									:matched_stations_to_display => nil}
+																										:matching_observation => nil}
 			else
-				# and this one is for queries with an id in the stations table, but nothing in the weather_data table
+				# and this one is for queries with an id in the stations table, but nothing in the weather_data table. example of this?
 				station = Station.from_table(station_record)
 
-				erb :index, :layout => :layout, :locals => {:station => nil,
-																									:station_to_match => nil,
-																									:station_to_match_data => nil,
-																									:matched_stations_to_display => []
+				erb :index, :layout => :layout, :locals => {:station => station,
+																									:matching_observation => nil
 																									}
 			end
 
 		else
 			# this is for when you have a station AND observation match, but only some of the data is missing. the page view tells you that
+			# my_station = matching_observation.station
 
-			station = Station.from_table(station_to_match)																			
-			station_to_match_data = Observation.from_table(station_to_match)
+			# station_to_match_data = Observation.from_table(station_to_match)
 			
-# this is making sure temp, dewpoint and weather conditions coded are present in the query station. can you find this out while querying the db?
-			if station_to_match_data.nil? || station_to_match_data.temp.nil? || station_to_match_data.dewpoint.nil? || station_to_match_data.weather_primary_coded.nil?
-				erb :index, :layout => :layout, :locals => {:station => station,
-																										:station_to_match => station_to_match,
-																										:station_to_match_data => nil,
+# this is making sure temp, dewpoint and weather conditions coded are present in the query station. can you find this out while querying the db? and, do you? at least if you'd like to display what data you actually have for the user
+			if matching_observation.temp.nil? || matching_observation.dewpoint.nil? || matching_observation.weather_primary_coded.nil?
+				
+				erb :index, :layout => :layout, :locals => {:matching_observation => matching_observation,
 																										:matched_stations_to_display => []}
 			else
 				
-				all_matches = station_to_match_data.find_matches
-				# i put the time check inside find_matches. is that better than having it here?
+				all_matches = matching_observation.find_matches('2015-07-02 00:00:00', '2015-07-02 02:00:00')
+				# i put the time check inside find_matches. is that better than having it here? yes. you want to remove as much logic as possible
+				# from your controller file
 
-				# matches_within_window = all_matches.reject do |ea|
-				# 	time_to_compare = time_threshold
-				# 	Time.parse(ea[:time]) <= time_to_compare
-				# end
-
-				checked_for_distance = all_matches.reject do |ea|
-					station_to_check = Station.from_table(ea)
-					station_to_check.too_close?(station)
-				end
-
-				matched_observations_to_display = checked_for_distance.map do |ea|
-					Observation.from_table(ea)
-				end
-				
-				matched_stations_to_display = checked_for_distance.map do |ea|
-					Station.from_table(ea)
-				end
-
-				match_observation_scores = matched_observations_to_display.map do |ea|
-					# observation = Observation.from_table(ea)
-					temp = ea.temp_score(station_to_match_data.temp)
-					dewpoint = ea.dewpoint_score(station_to_match_data.dewpoint)
-					humidity = ea.humidity_score(station_to_match_data.humidity)
-					wind_kph = ea.wind_kph_score(station_to_match_data.wind_kph)
-
-					total_score = (temp + dewpoint + humidity + wind_kph)/80.0
-					(total_score * 100).round(2)
+				unless all_matches.nil?
+					checked_for_distance = all_matches.reject do |ea|
+						station_to_check = Station.from_table(ea)
+						station_to_check.too_close?(matching_observation.station)
+					end
 					
-				end
+					# should make sure this also picks the most recent observation, in case there are 2 or more observations that fall within the window
+					# can you do a query that returns the most recent observation from the db?
+					checked_for_distance.uniq! { |match| match[:station_id] }
 
-				erb :index, :layout => :layout, :locals => {:station_to_match_data => station_to_match_data,
-																										:station => station,
-																										:station_to_match => station_to_match,
-																										:matched_stations_to_display => matched_stations_to_display,
-																										:matched_observations_to_display => matched_observations_to_display,
-																										:match_observation_scores => match_observation_scores }
+					matched_observations_to_display = checked_for_distance.map do |ea|
+						Observation.from_table(ea)
+					end
+
+					match_observation_scores = matched_observations_to_display.map do |ea|
+						temp = ea.temp_score(matching_observation.temp)
+						dewpoint = ea.dewpoint_score(matching_observation.dewpoint)
+						humidity = ea.humidity_score(matching_observation.humidity)
+						wind_kph = ea.wind_kph_score(matching_observation.wind_kph)
+
+						total_score = (temp + dewpoint + humidity + wind_kph)/80.0
+						(total_score * 100).round(2)
+						
+					end
+
+					erb :index, :layout => :layout, :locals => {:matching_observation => matching_observation,
+																											:matched_observations_to_display => matched_observations_to_display,
+																											:match_observation_scores => match_observation_scores }
+				end
 			end
 
 		end
@@ -139,11 +118,9 @@ get '/where_weather' do
 
 end
 
-# this populates the drop down with full location name using input from the user and matching with data from LOCATIONS
 get '/location_search' do
 
 	stations_table = DB[:stations]
-	# observations_table = DB[:weather_data]
 
   content_type :json
   query = params[:query]
