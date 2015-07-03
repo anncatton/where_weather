78d04cc448f3ec9e7d8a5e5cb3b100fbb5361a5f
Ann Catton <anncatton@gmail.com>
2015-07-03 14:54:57 -0400

    deleting old files. added time check back into find_matches.

--- models/observation.rb
+++ models/observation.rb
@@ -56,17 +56,15 @@ class Observation
 
 	def self.match_in_timeframe(station_id, start_time, end_time)
 
-
 		stations_and_observations_join = DB[:stations].join(DB[:weather_data], :station_id => :id)
-# where{time >= start_time}.where{time <= end_time}.
-		result = stations_and_observations_join.where(:station_id => station_id.upcase).first
+		result = stations_and_observations_join.where(:station_id => station_id.upcase).where{time >= start_time}.where{time <= end_time}.first
 		
 		if result 
 			from_table(result)
 		end
 	end
 
-	def find_matches
+	def find_matches(start_time, end_time)
 		# in this method, self is an Observation instance, not the whole db record for the query location
 		# this method returns an array of db records, not instances of Observation
 		stations_and_observations_join = DB[:stations].join(DB[:weather_data], :station_id => :id)
@@ -75,13 +73,12 @@ class Observation
 			:dewpoint => (dewpoint - 1)..(dewpoint + 1)).where(
 			:humidity => (humidity - 5)..(humidity + 5)).where(	
 			:weather_primary_coded => weather_primary_coded).where(
-			:wind_kph => (wind_kph - 5)..(wind_kph + 5)).exclude(
+			:wind_kph => (wind_kph - 5)..(wind_kph + 5)).where{
+			time >= start_time}.where{
+			time <= end_time}.exclude(
 			:station_id => station.id).all
 
-		time_matches = matches.reject do |ea|
-			ea[:time] <= (self.time - 3600)
-		end
-
+# somewhere in here maybe create an if/else for observations that are missing just some data values
 	end
 
 	def temp_score(query_temp)
@@ -147,58 +144,7 @@ class Observation
 
 		score
 	end
-	# this method is for finding the entry in stations table that matches what the user has entered on the website, so you have
-	# access to the station id (which is needed to locate it in observations). - Incorrect. this is using the id that is passed into
-	# the navigation bar to look in the observations table for that location's record in the database.
-	# in order for this to work, does this have to be an instance of the Observation class? that's what happens when you put a method
-	# inside a class, you can only call that method on instances of that class. but then, similar or same methods for different classes can
-	# have the same name, and ruby will know what to do for each one because it belongs to a class (.to_s is an example of this)
 
 end
 
-# DB = Sequel.connect('postgres://anncatton:@localhost:5432/mydb')
-
-# observations = DB[:weather_data]
-# stations = DB[:stations]
-
-# stations_data_for_user_id = stations.where(:id => my_id).first
-
-# def get_station_data(id,table)
-# 	table.where(:id => id).first
-# end
-
-# city_data = get_station_data(my_id, stations)
-
-# def find_city_id(city, table)
-# 	id = table.select(:id).where(:name=>city).first
-# end
-
-# puts find_city_id(my_city, stations)
-
-# to find the weather data associated with a station id
-# def find_station(id)
-# 	observations = DB[:weather_data]
-# 	match = observations.where(:station_id=>id.upcase).first
-# 	match
-# end
-
-# station_to_match = find_station(my_id)
-# station_to_match_data = Observation.from_table(station_to_match)
-
-
-# id_to_match = get_station_data(my_id, stations)
-# # so with too_close, you get all the stations that match, then, using their ids, check stations to see if they're too close
-# all_matches = matches?(station_to_match_data, observations)
-# match_data_array = all_matches.map do |ea|
-# 	Observation.from_table(ea)
-# end
-
-# match_data_ids = all_matches.map do |ea|
-# 	ea[:station_id]
-# end
-
-# # gives an array of station hashes from stations table
-# results = match_data_ids.map do |ea|
-# 	result = stations.where(:id=>ea).first
-# end
 
