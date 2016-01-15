12edf3e807bf1097450fc7a8cef70dbe0479f3a4
Ann Catton <anncatton@gmail.com>
2016-01-15 10:15:54 -0500

    making quotes consistent

--- public/js/global.js
+++ public/js/global.js
@@ -1,63 +1,62 @@
 $(document).ready(function() {
 
 	function dropMenu() {
-		$(".search-results").show();
+		$('.search-results').show();
 	};
 
 	function raiseMenu() {
-		$(".search-results").hide();
+		$('.search-results').hide();
 	};
 
-// this for the drop down menu
 	function populateDropDown(query) {
 		raiseMenu();
 		$.get('/location_search', {query: query}, function(data) {
-				$(".search-results").html($(data.html));
+				$('.search-results').html($(data.html));
 				dropMenu();	
 		});
 	};
 
 	var timeout;
- 	$(".location-input").keyup(function(event) {
+ 	$('.location-input').keyup(function(event) {
 
  		var $target = $(event.target);
- 		var $targetParent = $target.closest(".location-menu");
+ 		var $targetParent = $target.closest('.location-menu');
 
 // for down arrow
  		if (event.keyCode == 40) {
- 			var currentlySelectedListItem = $targetParent.find(".drop-down-item.selected");
+ 			var currentlySelectedListItem = $targetParent.find('.drop-down-item.selected');
  			var listItemToSelect = currentlySelectedListItem.next('.drop-down-item');
 
 // checks if selection is on last item in list
  			if ($(listItemToSelect).index() == -1) {
- 				listItemToSelect = $targetParent.find(".drop-down-item").first();
- 				$(currentlySelectedListItem).removeClass("selected");
- 				$(listItemToSelect).addClass("selected");
+ 				listItemToSelect = $targetParent.find('.drop-down-item').first();
+ 				$(currentlySelectedListItem).removeClass('selected');
+ 				$(listItemToSelect).addClass('selected');
  			}
 // for every other position on the list
  			else {
- 				$(currentlySelectedListItem).removeClass("selected");
- 				$(listItemToSelect).addClass("selected");
+ 				$(currentlySelectedListItem).removeClass('selected');
+ 				$(listItemToSelect).addClass('selected');
  			}
 
 // for up arrow navigation. when you use up arrow it also sends the input cursor back to the beginning
  		} else if (event.keyCode == 38) {
 
- 			var currentlySelectedListItem = $targetParent.find(".drop-down-item.selected");
+ 			var currentlySelectedListItem = $targetParent.find('.drop-down-item.selected');
  			var listItemToSelect = currentlySelectedListItem.prev('.drop-down-item');
 
  			if ($(currentlySelectedListItem).index() == 0) {
- 				$(currentlySelectedListItem).removeClass("selected");
- 				listItemToSelect = $targetParent.find(".drop-down-item").last();
- 				$(listItemToSelect).addClass("selected");
+ 				$(currentlySelectedListItem).removeClass('selected');
+ 				listItemToSelect = $targetParent.find('.drop-down-item').last();
+ 				$(listItemToSelect).addClass('selected');
  			} else {
- 				$(currentlySelectedListItem).removeClass("selected");
- 				$(listItemToSelect).addClass("selected");
+ 				$(currentlySelectedListItem).removeClass('selected');
+ 				$(listItemToSelect).addClass('selected');
  			}
 
  		} else if (event.keyCode == 13) {
- 			var currentlySelectedLink = $targetParent.find(".drop-down-item.selected a");
- 			window.location = currentlySelectedLink.attr("href");
+ 			var currentlySelectedLink = $targetParent.find('.drop-down-item.selected a');
+ 			window.location = currentlySelectedLink.attr('href');
  			
  		} else {
 
@@ -75,7 +74,7 @@ $(document).ready(function() {
  	});
 
 // raises menu before you can click link. will this matter once you have selection without cursor?
-	// $(".location_input").blur(function(event) {
+	// $('.location-input').blur(function(event) {
 	// 	raiseMenu();
 	// });
 
