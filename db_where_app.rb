4d706d2928540de8451d4e449db2ebf755559984
Ann Catton <anncatton@gmail.com>
2016-01-22 13:03:23 -0500

    time change

--- db_where_app.rb
+++ db_where_app.rb
@@ -6,6 +6,7 @@ require "byebug"
 require "pg"
 require "sequel"
 require "logger"
+require "ruby-prof"
 
 DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://anncatton:@localhost:5432/mydb')
 DB.loggers << Logger.new($stdout)
@@ -39,7 +40,8 @@ get '/where_weather' do
 
 	else
 
-		query_observation = Observation.match_in_timeframe(station_id, '2016-01-22 07:54:00', '2016-01-23 13:00:00')
+
+		query_observation = Observation.match_in_timeframe(station_id, '2016-01-22 12:00:00', '2016-01-23 13:00:00')
 
 		if query_observation.nil?
 
@@ -52,7 +54,7 @@ get '/where_weather' do
 
 		else
 
-				all_matches = query_observation.find_matches('2016-01-22 07:54:00', '2016-01-23 13:00:00')
+				all_matches = query_observation.find_matches('2016-01-22 12:00:00', '2016-01-23 13:00:00')
 
 				unless all_matches.nil?
 					matches_checked_for_distance = all_matches.reject do |ea|
@@ -118,12 +120,5 @@ get '/location_search' do
 	end
 
  { :html => content }.to_json
-
-end
-
-get '/admin_update_data' do
-
-	# the path where the form submit would go
-	# SELECT * FROM weather_data ORDER BY time DESC LIMIT 4584;
 
 end
